4d2c5f9cb393aad8ffce04fdf589c59f695f0d65
J-hta-n <e0969121@u.nus.edu>
2026-04-14 00:15:34 +0800

    feat: add settleup command handler to main telebot

--- src/bot/convo_handlers/Settleup/index.py
+++ src/bot/convo_handlers/Settleup/index.py
@@ -1,10 +1,11 @@
 from telegram.ext import CommandHandler
 
 from src.bot.convo_handlers.Base import BaseConversation
+from src.bot.convo_handlers.Settleup.flows.settleupFlow import settleup_command
 
 
 class Settleup(BaseConversation):
     def setup_handlers(self):
         self.entry_points = [
-            # (CommandHandler("settleup", set_expense_currency_command)),
+            (CommandHandler("settleup", settleup_command)),
         ]
